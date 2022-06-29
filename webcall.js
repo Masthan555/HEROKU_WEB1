80294d29ad52115f898f9d5c65fd5b0e6746c7c7
Masthan555 <masthan55591@gmail.com>
2022-06-29 13:15:15 +0530

    webca;;

---
+++ webcall.js
@@ -0,0 +1,24 @@
+/* Masthan Swamy */
+
+const https = require('http');
+
+const options = {
+  hostname: 'localhost',
+  port: 8081,
+  path: '/',
+  method: 'GET',
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', d => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+
+req.end();
